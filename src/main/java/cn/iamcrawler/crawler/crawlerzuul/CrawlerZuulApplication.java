c053276bee495bb4ee0aee0f5e6d25fc95f9a3aa
iamcrawler <iamcrawler@sina.com>
2019-02-28 19:22:27 +0800

    4

--- src/main/java/cn/iamcrawler/crawler/crawlerzuul/CrawlerZuulApplication.java
+++ src/main/java/cn/iamcrawler/crawler/crawlerzuul/CrawlerZuulApplication.java
@@ -2,10 +2,14 @@ package cn.iamcrawler.crawler.crawlerzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableZuulProxy
 public class CrawlerZuulApplication {
 
